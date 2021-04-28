665514d5b348696daca8f38fbd3f5f8489afc052
compuvin <compuvin@users.noreply.github.com>
2021-04-28 13:35:12 -0400

    New module
    New Nexpose API module and code commenting on SEP module

--- Modules/Nexpose.ps1
+++ Modules/Nexpose.ps1
@@ -5,8 +5,8 @@
 ##################################
 #Edit these values as you see fit
 $PSSchema = "hardwaremvp"
-$PSTbl = "SymantecEndpointManager"
-$PSFN = "Symantec Endpoint Manager"
+$PSTbl = "nexpose"
+$PSFN = "Rapid7 Nexpose"
 $PSRunInt = 1
 $PSML = 0
 ##################################
@@ -40,12 +40,13 @@ function Get-IniContent ($filePath)
 
 $ModuleFolder = Split-Path -Parent -Path $MyInvocation.MyCommand.Source
 $IniContent = Get-IniContent ((Split-Path -Parent -Path $ModuleFolder) + "\smapp.ini")
-$SEPData = New-Object System.Collections.ArrayList
+$NexposeData = New-Object System.Collections.ArrayList
 $UpdateDate = Get-Date -Format "yyyy-MM-dd"
+$DomainName = (Get-WmiObject Win32_ComputerSystem).Domain
 $outputl = ""
 
 
-[String] $Server = $IniContent[$PSTbl]["SEPServer"]
+[String] $Server = $IniContent[$PSTbl]["NexposeServer"]
 
 #Check to make sure that the module info is in the INI
 #We'll store this in the database at some point, don't worry
@@ -54,29 +55,26 @@ if (!$Server)
 	$outputl = "<p>Additional information is needed for the " + $PSFN + " module to run. Please edit the smapp.ini file located at: " + (Split-Path -Parent -Path $ModuleFolder) + "</p>
 		<p>Please copy and paste the lines below and edit the values as indicated:</p><p>
 		[" + $PSTbl + "]<br>
-		SEPServer=#YourServerHere#<br>
-		SEPUser=#YourUserHere#<br>
-		SEPPassword=#YourPasswordHere#<br>
-		SEPDomain=#YourDomainHere#"
+		NexposeServer=#YourServerHere#<br>
+		NexposeUser=#YourUserHere#<br>
+		NexposePassword=#YourPasswordHere#"
 	Send-MailMessage -From $IniContent["Email"]["RptFromEmail"] -To $IniContent["Email"]["RptToEmail"] -SmtpServer $IniContent["Email"]["EmailSvr"] -Subject ("HardwareMVP: Action Required - " + $PSFN) -Body $outputl -BodyAsHtml
 	Break
 }
 
-# Create credentiaal and convert to JSON
-$cred = @{
-    username = $IniContent[$PSTbl]["SEPUser"]
-    password = $IniContent[$PSTbl]["SEPPassword"]
-    domain   = $IniContent[$PSTbl]["SEPDomain"]
-}
-$auth = $cred | ConvertTo-Json
+# Create credentiaal and convert to Base64String
+$username = $IniContent[$PSTbl]["NexposeUser"]
+$password = $IniContent[$PSTbl]["NexposePassword"]
 
+[string]$authInfo = ([Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(('{0}:{1}' -f $username, $password))))
 
-$authToken = (Invoke-RestMethod -Uri https://$($Server)/sepm/api/v1/identity/authenticate -Method Post -Body $auth -ContentType 'application/json')
 
 # format HTTP header
-$header = @{ Authorization='Bearer '+ $authToken.Token }
+$header = @{ Authorization='Basic {0} ' -f $authInfo }
 
-$SEPData = (Invoke-RestMethod -Uri https://$($Server)/sepm/api/v1/computers -Headers $header -Body @{ pageSize='1000' }) | select -ExpandProperty content | select computerName, ipAddresses
+$SearchFilter = '{"match": "all","filters": [{ "field": "operating-system", "operator": "contains", "value": "Microsoft" }]}'
+
+$NexposeData = (Invoke-RestMethod -Uri https://$($Server)/api/3/assets/search?size=10000 -Method Post -Headers $header -Body $SearchFilter -ContentType 'application/json') | select -ExpandProperty resources | select hostname, ip, os
 
 
 #Open MySQL Connection
@@ -86,22 +84,25 @@ $myconnection.Open()
 $command = $myconnection.CreateCommand()
 
 #Create table for this module if it doesn't exist
-$command.CommandText = "CREATE TABLE IF NOT EXISTS " + $PSSchema + "." + $PSTbl + " (ID INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(255) UNIQUE, IPAddress text, FirstDiscovered date DEFAULT NULL, LastDiscovered date DEFAULT NULL)";
+$command.CommandText = "CREATE TABLE IF NOT EXISTS " + $PSSchema + "." + $PSTbl + " (ID INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(255) UNIQUE, IPAddress text, SiteName text, OS text, FirstDiscovered date DEFAULT NULL, LastDiscovered date DEFAULT NULL)";
 $reader = $command.ExecuteNonQuery()
 
-#Update the settings for this module using the variables set at the top of the script
+#Update the settings for this module using the variables set at the top of the script.
 $command.CommandText = "UPDATE " + $PSSchema + ".modules SET `Name` = '" + $PSFN + "', `TableName` = '" + $PSTbl + "', `RunInterval` = '" + $PSRunInt + "', `MasterList` = '" + $PSML + "' WHERE (`FileName` = '" + $MyInvocation.MyCommand.Name + "')";
 $reader = $command.ExecuteNonQuery()
 
 #Add/Update table entries
-foreach ($SEPRow in $SEPData)
+foreach ($NexposeRow in $NexposeData)
 {
-	$command.CommandText = "INSERT INTO " + $PSSchema + "." + $PSTbl + "(Name, IPAddress, FirstDiscovered, LastDiscovered) values('" + $SEPRow.computerName.ToUpper() + "','" + $SEPRow.ipAddresses + "','" + $UpdateDate + "','" + $UpdateDate + "') ON DUPLICATE KEY UPDATE IPAddress='" + $SEPRow.ipAddresses + "', LastDiscovered='" + $UpdateDate + "'";
-	$reader = $command.ExecuteNonQuery()
+	if ($NexposeRow.hostname -ne "" -and $NexposeRow.hostname) #Ignore empty device names
+	{
+		$command.CommandText = "INSERT INTO " + $PSSchema + "." + $PSTbl + "(Name, IPAddress, SiteName, OS, FirstDiscovered, LastDiscovered) values('" + $NexposeRow.hostname.replace(("." + $DomainName),"").ToUpper() + "','" + $NexposeRow.ip + "','','" + $NexposeRow.os + "','" + $UpdateDate + "','" + $UpdateDate + "') ON DUPLICATE KEY UPDATE IPAddress='" + $NexposeRow.ip + "', OS='" + $NexposeRow.os + "', LastDiscovered='" + $UpdateDate + "'";
+		$reader = $command.ExecuteNonQuery()
+	}
 }
 
 #Remove old entries
-if ($SEPData.length -gt 0)
+if ($NexposeData.length -gt 0)
 {
 	$command.CommandText = "DELETE FROM " + $PSSchema + "." + $PSTbl + " WHERE NOT (`LastDiscovered` = '" + $UpdateDate + "')";
 	$reader = $command.ExecuteNonQuery()
@@ -119,6 +120,7 @@ if ($SEPData.length -gt 0)
 #  }
 #}
 
+#Send email
 if ($outputl -ne "") { Send-MailMessage -From $IniContent["Email"]["RptFromEmail"] -To $IniContent["Email"]["RptToEmail"] -SmtpServer $IniContent["Email"]["EmailSvr"] -Subject "HardwareMVP: " + $PSFN + " Report" -Body $outputl -BodyAsHtml }
 
 #Close MySQL Connection
